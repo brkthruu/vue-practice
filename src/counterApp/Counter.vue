4aa2cb9a9175f8f9398198300d470004c6011588
hysimok <jeonghy95@gmail.com>
2021-09-27 16:10:46 +0900

    complete Counter
    add action

--- src/counterApp/Counter.vue
+++ src/counterApp/Counter.vue
@@ -2,8 +2,11 @@
 	<div>
     <h1>COUNTER</h1>
     <div>{{count}}</div>
+		<div>{{time2}}</div>
     <button @click="increment">UP VOTE</button>
 		<button @click="decrement">Down VOTE</button>
+		<button @click="incrementIfOdd">Increment If Odd</button>
+		<button @click="incrementAsync">Increment Async</button>
   </div>
 </template>
 
@@ -18,8 +21,13 @@ export default {
 
 		return { 
 			count: computed(() => store.state.count ),
-			increment: () => store.commit('increment'),		// mutation 호출
-			decrement: () => store.commit('decrement'),
+			// increment: () => store.commit('increment'),		// mutation 호출
+			// decrement: () => store.commit('decrement'),
+			time2: computed(() => store.getters.time2),
+			increment: () => store.dispatch('increment'),
+			decrement: () => store.dispatch('decrement'),
+			incrementIfOdd: () => store.dispatch('incrementIfOdd'),
+			incrementAsync: () => store.dispatch('incrementAsync')
 		}
 	}
 }
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ export default createStore({
 	},
 	getters: {
 		time2(state) {
-			return state.counter * 2;
+			return state.count * 2;
 		}
 	},
 	mutations: {
@@ -15,6 +15,23 @@ export default createStore({
 		},
 		decrement(state) {
 			state.count--;
+		}
+	},
+	actions: {
+		increment: ({ commit }) => commit('increment'),
+		decrement: ({ commit }) => commit('decrement'),
+		incrementIfOdd ({ commit, state }) {
+			if ((state.count + 1) % 2 === 0) {
+				commit('increment')
+			}
+		},
+		incrementAsync ({ commit }) {
+			return new Promise((resolve) => {
+				setTimeout(() => {
+					commit('increment')
+					resolve()
+				}, 1000)	//1초 후 증가
+			})
 		}
 	}
 })
